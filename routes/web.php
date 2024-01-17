55fa1e46e071b4ed8f3791cff150016793de8a2f
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-01-17 18:23:32 +0200

    update routing

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
