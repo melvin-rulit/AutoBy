be942f843d9bf0fa5a08b7ef80c24c88b3ca45ea
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-01-17 15:14:55 +0200

    update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,10 +16,20 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->resource->id,
+            'email' => $this->resource->email,
             'first_name' => $this->resource->first_name,
             'middle_name' => $this->resource->middle_name,
             'last_name' => $this->resource->last_name,
             'birthday' => $this->resource->birthday,
+            'passport_series',
+            'passport_number' => $this->resource->passport_number,
+            'passport_notes' => $this->resource->passport_notes,
+            'registration_address' => $this->resource->registration_address,
+            'phone_number' => $this->resource->phone_number,
+            'comment' => $this->resource->comment,
+            'role_id' => $this->resource->role_id,
+            'branch_id' => $this->resource->branch_id,
+            'enabled' => $this->resource->enabled,
         ];
     }
 }
