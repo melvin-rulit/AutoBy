3aafa45ff7c8f35e46ff3d83df9482d63744061b
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-01-17 22:19:22 +0200

    create app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+
+import {createApp} from 'vue'
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+import App from "./components/App.vue";
+import UserList from "./components/users/UserList.vue";
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            component: UserList
+        },
+    ]
+})
+
+createApp(App).use(router).mount('#app')
