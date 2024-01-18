e46d132d25fe691003dd3d61af5002b70b965355
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-01-18 10:16:29 +0200

    add table for customer users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\UserResource;
-use Illuminate\Http\JsonResponse;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
+
+    /**
+     * UserController constructor.
+     */
+    public function __construct(private User $users)
+    {
+    }
+
+   public function index(Request $request): JsonResponse
+    {
+        $user = auth()->user();
+
+        if ($user->isAdmin()){
+            $users = $this->users;
+        } else {
+            $users = $this->users->where('branch_id', $user->getBranchId());
+        }
+
+        return new JsonResponse(['users' => $users]);
+    }
+
     public function current(): JsonResponse
     {
        return new JsonResponse(['user' => UserResource::make(auth()->user())]);
+    }
+
+    public function getRoles(): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'roles' => [
+
+                ]
+            ]
+        );
+
+
     }
 
 }
