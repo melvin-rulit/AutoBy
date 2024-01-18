6a9a48af94220f1ca47bcc8185d8ab85bbc1b932
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-01-18 09:46:01 +0200

    update Header.vue

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,12 +1,24 @@
 <template>
-<div>
-    <h1>LOPATA</h1>
-</div>
+    <nav className="fixed z-30 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div className="px-3 py-1 lg:px-5 lg:pl-3">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center justify-start">
+
+                </div>
+
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
 export default {
-    name: "Header"
+    name: "Header",
+    data: function () {
+        return {
+
+        }
+    },
 }
 </script>
 
