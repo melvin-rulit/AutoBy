24bc2bbcd97bc74b7ba0037e658f6cc02aeb797e
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-01-26 21:08:44 +0200

    create proxi files

---
+++ resources/js/services/ProxiService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export class ProxiService {
+    static serverUrl = import.meta.env.VITE_BASE_URL;
+
+    static getProxies(query){
+        let url = `${this.serverUrl}/proxies`;
+        return axios.get(url, {"params": {"query": query}});
+    }
+    static store(proxi) {
+        let url = `${this.serverUrl}/proxies`;
+        return axios.post(url, proxi)
+    }
+
+    static getById(id){
+        let url = `${this.serverUrl}/proxies/${id}`;
+        return axios.get(url);
+    }
+
+    static update(proxi){
+        let url = `${this.serverUrl}/proxies/${proxi.id}`;
+        return axios.put(url, proxi);
+    }
+
+    static delete(id) {
+        let url = `${this.serverUrl}/proxies/${id}`;
+        return axios.delete(url, id)
+    }
+}
